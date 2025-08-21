9c8787922b4843ef33adadebacdabf9ddf8533ae
pass42 <dnop030@gmail.com>
2025-08-21 21:43:12 +0700

    ClangFormatEx08ButNotClean

--- ex08/srcs/ex08.c
+++ ex08/srcs/ex08.c
@@ -11,14 +11,18 @@ MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 static ssize_t myfd_read(struct file *fp, char __user *user, size_t size,
 			 loff_t *offs);
+
 static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
 			  loff_t *offs);
+
 static struct file_operations myfd_fops = { .owner = THIS_MODULE,
 					    .read = &myfd_read,
 					    .write = &myfd_write };
+
 static struct miscdevice myfd_device = { .minor = MISC_DYNAMIC_MINOR,
 					 .name = "reverse",
 					 .fops = &myfd_fops };
+
 char str[PAGE_SIZE];
 // char *tmp;
 static int __init myfd_init(void)
@@ -37,19 +41,18 @@ ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
 	size_t t, i;
 	char *tmp2;
-	ssize_t	ret;
+	ssize_t ret;
 	/***************
 	* Malloc like a boss
 	***************/
 	// tmp2 = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
 	tmp2 = kmalloc(sizeof(char) * PAGE_SIZE, GFP_KERNEL);
-	if (!tmp2)
-	{
+	if (!tmp2) {
 		printk(KERN_INFO "useless rd fn malloc failed\n");
 		return -ENOMEM;
 	}
 	// tmp = tmp2;
-	printk(KERN_INFO "useless rd fn msg len:%zd msg:%s\n", strlen(str), str);
+	// printk(KERN_INFO "useless rd fn msg len:%zd msg:%s\n", strlen(str), str);
 	// for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++) {
 	for (t = strlen(str) - 1, i = 0; i < strlen(str); t--, i++) {
 		// printk(KERN_INFO "useless rd fn index:%zu\n", t);
