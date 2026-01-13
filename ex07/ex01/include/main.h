3b77f86565e5dd73be6985cd9a622eafccd1a984
pass42 <dnop030@gmail.com>
2026-01-13 15:58:57 +0700

    ex07
    main.h
    -add prototype function

--- ex07/ex01/include/main.h
+++ ex07/ex01/include/main.h
@@ -6,6 +6,14 @@
 #include <linux/debugfs.h>
 #include <linux/jiffies.h>
 
+ssize_t foo_read (struct file *filp, char __user *usr_spac_buff, size_t count, loff_t *offset);
+ssize_t foo_write (struct file *filp, const char __user *usr_spac_buff, size_t count, loff_t *offset);
+
+ssize_t jiffies_file_read (struct file *filp, char __user *usr_spac_buff, size_t count, loff_t *offset);
+
+ssize_t id_read (struct file *filp, char __user *usr_spac_buff, size_t count, loff_t *offset);
+ssize_t id_write (struct file *filp, const char __user *usr_spac_buff, size_t count, loff_t *offset);
+
 int driver_init(void);
 void	driver_exit(void);
 
