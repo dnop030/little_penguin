dbacc239d42005a2f6281419a0642bc1984bc6ac
pond@lfs <pond@lfs.com>
2026-01-16 00:58:16 +0700

    ex01
    apply coding style

--- ex01/srcs/main.c
+++ ex01/srcs/main.c
@@ -1,14 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 #include "../include/main.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("psrikamo");
 
-int	__init driver_init(void) {
+int	__init driver_init(void)
+{
 	printk(KERN_INFO "Hello world!\n");
 	return 0;
 }
 
-void	__exit driver_exit(void) {
+void	__exit driver_exit(void)
+{
 	printk(KERN_INFO "Cleaning up module.\n");
 }
 
