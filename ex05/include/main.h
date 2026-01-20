57b5e58b26426be51b4072504ba02342069c5837
pond@lfs <pond@lfs.com>
2026-01-20 14:43:23 +0700

    Revert "ex05"
    This reverts commit 2ce3710c1a944a504222d7b789b242c5b4d3e02e.

--- ex05/include/main.h
+++ ex05/include/main.h
@@ -1,4 +1,3 @@
-/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef MAIN_H
 # define MAIN_H
 
@@ -6,10 +5,10 @@
 #include <linux/module.h>
 #include <linux/miscdevice.h>
 
-ssize_t fortwo_read(struct file *filp, char __user *usr_spac_buff, size_t count, loff_t *offset);
-ssize_t fortwo_write(struct file *filp, const char __user *usr_spac_buff, size_t count, loff_t *offset);
-int fortwo_open(struct inode *inode, struct file *filp);
-int fortwo_release(struct inode *inode, struct file *filp);
+ssize_t fortwo_read (struct file *filp, char __user *usr_spac_buff, size_t count, loff_t *offset);
+ssize_t fortwo_write (struct file *filp, const char __user *usr_spac_buff, size_t count, loff_t *offset);
+int fortwo_open (struct inode *inode, struct file *filp);
+int fortwo_release (struct inode *inode, struct file *filp);
 
 int fortwo_init(void);
 void	driver_exit(void);
